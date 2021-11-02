4193cbc8d13e116330b3ddfe0d2c96af987f1512
Tanaka.1003 <tatsuya1003@outlook.jp>
2021-11-02 08:11:10 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books
   
-  resources :books, only: [:new,:creat,:index,:show,:destroy] do
+  resources :books, only: [:new,:create,:index,:show,:destroy] do
     resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create,:destroy]
   end
